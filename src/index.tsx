f3a4aa70d6b1011634b19f54daa95312420abcb6
kuromon11 <yoshikikuro712@gmail.com>
2024-09-08 17:16:53 +0900

    feat: #4 react-routerを使って複数ページを参照できるよう修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'; // ファイル内で使用しないが、JSXを使う場合はインポートの必要があるため必須
 import { render } from 'react-dom';
 import { createGlobalStyle } from 'styled-components';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 import { Editor } from './pages/editor';
 
@@ -14,7 +20,15 @@ const GlobalStyle = createGlobalStyle`
 const Main = (
   <>
     <GlobalStyle />
-    <Editor />
+    <Router>
+      <Route exact path="/editor">
+        <Editor />
+      </Route>
+      <Route exact path="/history">
+        <h1>History</h1>
+      </Route>
+      <Redirect to="/editor" path="*" />
+    </Router>
   </>
 );
 
