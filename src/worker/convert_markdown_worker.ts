1496fd7188a30a65bce44827422d9671b38f2774
kuromon11 <yoshikikuro712@gmail.com>
2024-09-21 16:35:06 +0900

    feat: #5 HTMLのサニタイズを実装

--- src/worker/convert_markdown_worker.ts
+++ src/worker/convert_markdown_worker.ts
@@ -1,9 +1,13 @@
 import * as marked from 'marked';
+import * as sanitizeHtml from 'sanitize-html';
 
 const worker: Worker = self as any;
 
 worker.addEventListener('message', (event) => {
   const text = event.data;
-  const html = marked(text);
+  // HTMLをサニタイズ
+  const html = sanitizeHtml(marked(text), {
+    allowedTags: [...sanitizeHtml.defaults.allowedTags, 'h1', 'h2'],
+  });
   worker.postMessage({ html });
 });
